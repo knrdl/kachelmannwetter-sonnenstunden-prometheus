a20cfccca6f1d76de75ebb8a2f5b04d1a4903515
knrdl <35548889+knrdl@users.noreply.github.com>
2025-07-23 20:38:09 +0200

    fix: add more request headers in order to prevent 403 responses

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
-FROM python:3.13.3-alpine3.20
+FROM python:3.13.5-alpine3.21
 
 ENV PYTHONIOENCODING=utf-8
 ENV PYTHONUNBUFFERED=1
 
 RUN adduser --home /home/app --disabled-password --shell /bin/false --uid 1000 app
+
+RUN pip install --no-cache-dir requests
 
 CMD ["python3", "main.py"]
 
--- metrics.py
+++ metrics.py
@@ -1,10 +1,15 @@
-from urllib.request import urlopen
-from urllib.parse import urlencode
+import requests
 import time
 import json
 
 _cache_value: str | None = None
 _cache_ts: int | None = None
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:140.0) Gecko/20100101 Firefox/140.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+}
+ses = requests.Session()
 
 def generate(area_id: int, station_id: str) -> str:
     global _cache_value, _cache_ts
@@ -15,18 +20,22 @@ def generate(area_id: int, station_id: str) -> str:
         _cache_ts = now
 
         output = ''
-        resp = urlopen('https://kachelmannwetter.com/de/ajax/obsdetail?' + urlencode({
-            'model': 'obs',
-            'param_id': '64',
-            'counter': 'false',
-            'lang': 'DE',
-            'station_id': station_id, 
-            'timestamp': time.strftime('%Y%m%d%H00'), 
-            'area_id': str(area_id)
-        }))
-        if resp.code != 200:
-            raise Exception(f'wrong response code: {resp.code}')
-        records: list[dict[str, int]] = json.loads(resp.read().decode().split(' var hc_obs_series = ')[1].split(';')[0])[0]['data']
+
+        resp = ses.get('https://kachelmannwetter.com/de/messwerte/stadt-berlin/sonnenstunden/20250723-0600z.html', headers=headers)
+
+        resp = ses.get('https://kachelmannwetter.com/de/ajax/obsdetail', headers=headers, params={
+                'model': 'obs',
+                'param_id': '64',
+                'counter': 'false',
+                'lang': 'DE',
+                'station_id': station_id, 
+                'timestamp': time.strftime('%Y%m%d%H00'), 
+                'area_id': str(area_id),
+            })
+
+        if resp.status_code != 200:
+            raise Exception(f'wrong response code: {resp.status_code}')
+        records: list[dict[str, int]] = json.loads(resp.text.split(' var hc_obs_series = ')[1].split(';')[0])[0]['data']
         for record in records:
             timestamp = record['x']
             value = record['y']
